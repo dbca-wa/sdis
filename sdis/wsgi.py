9ffe3d7d9d322c371a712f7a01052734ab6ca662
florianm <florian.mayer@dpaw.wa.gov.au>
2016-01-25 15:05:45 +0800

    hook django-confy into fabfile and wsgi

--- sdis/wsgi.py
+++ sdis/wsgi.py
@@ -3,15 +3,15 @@ WSGI config for sdis project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+@see https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+@see https://github.com/kennethreitz/dj-static
+@see http://django-confy.readthedocs.org/en/latest/usage.html
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sdis.settings")
-
-
+import confy
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from dj_static import Cling, MediaCling
 
-application = Cling(get_wsgi_application())
+confy.read_environment_file()
+application = Cling(MediaCling(get_wsgi_application()))
+
