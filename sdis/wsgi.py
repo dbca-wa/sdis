50cd38883e23d45b53128d319ec671d454e6790b
florianm <florian.mayer@dpaw.wa.gov.au>
2016-04-18 09:50:00 +0800

    modified manage.py and wsgi.py to dpaw-utils suggested use of django-confy

--- sdis/wsgi.py
+++ sdis/wsgi.py
@@ -6,7 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
+import confy
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
+import os
 
+confy.read_environment_file('.env')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sdis.settings")
 application = Cling(MediaCling(get_wsgi_application()))
