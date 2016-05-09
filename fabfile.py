e3c5e3b4787c85615c503dd7650a016a4e9b8a2f
florianm <florian.mayer@dpaw.wa.gov.au>
2016-05-09 10:28:52 +0800

    fine tuned coverage settings

--- fabfile.py
+++ fabfile.py
@@ -115,8 +115,9 @@ def test():
     """Write PEP8 warnings to logs/pep8.log and run test suite, re-use db."""
     print(yellow("Running tests..."))
     # local('python manage.py test --keepdb -v 2') # django 1.8
-    local('coverage run --source="." manage.py test '
-          '--ipdb --settings=sdis.test_settings')
+    local('coverage run --source="." manage.py test --ipdb '
+          '--settings=sdis.test_settings && coverage report -m',
+          shell='/bin/bash')
     print(green("Completed running tests."))
 
 
