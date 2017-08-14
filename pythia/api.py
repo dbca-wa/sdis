a6052967b97374520b708a298c5be20934d15d1c
florianm <florian.mayer@dpaw.wa.gov.au>
2017-08-14 16:34:06 +0800

    add api docs

--- pythia/api.py
+++ pythia/api.py
@@ -11,20 +11,43 @@ from rest_framework.authentication import (
     SessionAuthentication, BasicAuthentication, TokenAuthentication)
 
 from pythia.models import Program, WebResource, Division, Area, User
-# from pythia.projects.models import ()
-# from pythia.document.models import ()
-# from pythia.reports.models import ()
+from pythia.projects.models import (
+    Project, ScienceProject, CoreFunctionProject, CollaborationProject,
+    StudentProject)
+from pythia.documents.models import (
+    Document, ConceptPlan, ProjectPlan, ProgressReport, ProjectClosure,
+    StudentReport)
+from pythia.reports.models import (ARARReport)
 
 
+# -----------------------------------------------------------------------------#
+# Serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'is_staff')
 
 
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'type', 'year', 'number', 'status',
+                  'title', 'tagline', 'image')
+
+
+# -----------------------------------------------------------------------------#
+# Viewsets
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+# -----------------------------------------------------------------------------#
+# Routers
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'projects', ProjectViewSet)
--- sdis/urls.py
+++ sdis/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
+# from rest_framework_swagger.views import get_swagger_view
 
 from pythia.sites import site
 from pythia.views import TermsAndConditions, spell_check
 from pythia.api import router
 
 js_info_dict = {'packages': ('django.conf',), }
+
+# schema_view = get_swagger_view(title='SDIS API')
 
 urlpatterns = patterns(
     '',
@@ -25,6 +28,7 @@ urlpatterns = patterns(
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-docs/', include('rest_framework_swagger.urls')),
 
     url(r'^export/(?P<model_name>[^/]+)/$',
         "django_tablib.views.generic_export"),
