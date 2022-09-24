7d13ce8b9037f5b91bf4616bff81aae32b42f92d
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-09-24 13:03:39 +0800

    Drop spellcheck import

--- sdis/urls.py
+++ sdis/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 # from rest_framework_swagger.views import get_swagger_view
 
 from pythia.sites import site
-from pythia.views import TermsAndConditions, spell_check
+from pythia.views import TermsAndConditions #, spell_check
 from pythia.api import router
 
 js_info_dict = {'packages': ('django.conf',), }
